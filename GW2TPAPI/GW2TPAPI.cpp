e4e1d80d522455d1630ff2b2e1560a5594ae9626
karusb <sokucam@hotmail.com>
2017-10-22 21:21:10 +0300

    Changes to JSON parser functions that also takes in to account new lines while calculating the end of data position. New database aquisition system 100x faster than the previous implementation. Rebuilding database now takes 2 minutes. This is implemented with the new function InitJSON_ITEMSDB. ITEM_REQ_COUNT defined globally which determines the response per request, this is hard limited to 200 by the API.

--- GW2TPAPI/GW2TPAPI.cpp
+++ GW2TPAPI/GW2TPAPI.cpp
@@ -1,5 +1,5 @@
 #include "stdafx.h"
-#include <string.h>
+
 #include <windows.h>
 #include <iostream>
 #include <vector>
@@ -17,28 +17,33 @@
 //#define _WINSOCK_DEPRECATED_NO_WARNINGS_
 using namespace std;
 #pragma comment(lib,"ws2_32.lib")
-#define ITEM_COUNT 24465
+#define ITEM_COUNT 24476
+#define ITEM_REQ_COUNT 100
 int SELLQ_Limit=550;
 int BUYQ_Limit=550;
+int BUYG_MIN = 0;
+int BUYG_MAX = 9999999;
 
 char buffer[10000];
 string website_HTML;
 string stringresponse;
-////////////////////////
-// Constant strings
-const string API_KEY = "F0415A36-8665-6049-AE2A-F97254BABE69E79410B8-61C4-49E3-B09D-693344F57B30";
+
 const string API_Link = "api.guildwars2.com/v2"; // ADD all links in a class?
+
 //****************************************************
 // Functions
-int getstringinputsizeJSON(string *inputfile, string tobefound, string tobefoundwithin = "?");
-int getstringinputpositionJSON(string *inputfile, string tobefound, string tobefoundwithin = "?");
-string ParseJSON(string *inputfile, string tobefound, string tobefoundwithin = "?");
+
+int getstringinputsizeJSON(string *inputfile, string tobefound, string tobefoundwithin = "?", string tobefoundwithinfirst = "?");
+int getstringinputpositionJSON(string *inputfile, string tobefound, string tobefoundwithin = "?", string tobefoundwithinfirst ="?");
+string ParseJSON(string *inputfile, string tobefound, string tobefoundwithin = "?", string tobefoundwithinfirst = "?" );
 item ParseJSON_ITEM(int id);
+void InitJSON_ITEMSDB(item* items,int* itemslist,int itempos);
 string ParseJSON_ITEMName(int it);
 void get_Website(char *url);
 size_t CURLWrite_Callback_String(void *contents, size_t size, size_t nmemb, std::string *s);
 void itemlistload(string filename,int *itemlist);
 void itemdbload(string filename, item *itemdb);
+int itemdbintegrity(string filename);
 
 int main(void) {
 	////////////////////////
@@ -48,18 +53,28 @@ int main(void) {
 	int favj = 0;
 	bool favselect = false;
 	item items[ITEM_COUNT];
-	item favitems[5000];
+	item favitems[10000];
 	item user_favitems[50];
 	int *itemslist;
 	ofstream itemsout;
 	ofstream itemtpout;
+	ifstream APIin;
 	itemslist = new int[ITEM_COUNT];
 	string user_in = "0";
 	//string EXAMPLE = "\{ /r/n\"id\": 19684,/r/n\"whitelisted\" : false,/r/n\"buys\" : {/r/n	\"quantity\": 145975,/r/n		\"unit_price\" : 7018},/r/n\"sells\" : {/r/n		\"quantity\": 126,/r/n			\"unit_price\" : 7019/r/n	}}";
 	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	// Below is the main code for checking the IP in intervals.
-	itemlistload("items.txt", itemslist);
+	//// READ API KEY FROM FILE
+	//APIin.open("APIkey.txt");
+	//if (!APIin.is_open()) cout << "APIKey.txt was not read" << endl;
+	//APIin >> API_KEY;
+	//APIin.close();
+	//if (API_KEY == "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXXXXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX") cout << "Please provide your API KEY in the APIKey.txt file" << endl;
+	//
 
+	// LOAD THE LIST OF ITEMS TO BE FETCHED
+	itemlistload("items.txt", itemslist);
+	
+	// UI BEGIN 
 	cout << "Rebuild DB? (Y/N) " << endl;
 	cin >> user_in;
 	if (user_in == "n" || user_in == "N")
@@ -71,47 +86,78 @@ int main(void) {
 		//for (int i = 0; i < 300; i++)cout << itemslist[i];
 		cout << "\n\n\n";
 		// CONTROL DATABASE - ONLY REBUILD DB IF REQUESTED (user control)
-		requestitem = 19684;
+		//requestitem = 19684;
+		//itemsout.open("itemsdb.txt");
+		//for (int i = 0; i < ITEM_COUNT; i++)
+		//{
+		//	requestitem = itemslist[i];
+		//	items[i] = ParseJSON_ITEM(requestitem);
+		//	cout << items[i] << endl;
+		//	itemsout << items[i] << endl;
+		//}
+		//itemsout.close();
+
+		for (int i = 0; i < ITEM_COUNT; i += ITEM_REQ_COUNT)
+		{
+			InitJSON_ITEMSDB(items, itemslist, i);
+		}
+		cout << " Download Complete..." << endl;
 		itemsout.open("itemsdb.txt");
 		for (int i = 0; i < ITEM_COUNT; i++)
 		{
-			requestitem = itemslist[i];
-			items[i] = ParseJSON_ITEM(requestitem);
-			cout << items[i] << endl;
-			itemsout << items[i] << endl;
+				itemsout << items[i] << endl;
 		}
 		itemsout.close();
+		cout << " Database saved successfully..." << endl;
 	}
 
-		
-		std::sort(items, items + ITEM_COUNT);
-		itemtpout.open("TP.txt");
-		for (int i = 0; i < ITEM_COUNT; i++)
-		{
-			if(items[i].buyquantity() >= BUYQ_Limit && items[i].sellquantity() >= SELLQ_Limit)itemtpout << items[i] << endl;
-		}
-		itemtpout.close();
+	if (itemdbintegrity("itemsdb.txt") == ITEM_COUNT) cout << "INTEGRITY CHECK PASSED" << endl;
+	else cout << itemdbintegrity("itemsdb.txt") << endl;
 
-		cout << " [1] LIST [2] LIVE UPDATE FAV [3] SELECT FAVOURITES [4] CHANGE SELL QUANTITY LIMITS [5] REBUILD DATABASE [esc] QUIT" << endl;
+		std::sort(items, items + ITEM_COUNT);
+		
+		
+		
+
+		cout << " [1] LIST [2] LIVE UPDATE FAV [3] SELECT FAVOURITES [4] CHANGE LIMITS [5] REBUILD DATABASE [esc] QUIT" << endl;
 		cin >> user_in;
 		do
 		{
 			if (user_in == "1")
 			{
-				cout << "How many items to parse? ";
+				cout << "How many items to show? ";
 				cin >> user_in;
 				int parsesize = stoi(user_in);
-			
+				itemtpout.open("TP.txt");
 				for (int i = 0; i < ITEM_COUNT; i++)
 				{
-					if (items[i].buyquantity() >= BUYQ_Limit && items[i].sellquantity() >= SELLQ_Limit)
+					if (items[i].buyquantity() >= BUYQ_Limit && items[i].sellquantity() >= SELLQ_Limit && items[i].buyprice() <= BUYG_MAX && items[i].buyprice() >= BUYG_MIN)
 					{
 						favitems[favj] = items[i];
 						favj += 1;
+						itemtpout << items[i] << endl;
 					}
 				}
+				itemtpout.close();
 				for (int i =0; i < parsesize && i < favj;i++)cout << "[ " << i <<" ]"<< ParseJSON_ITEMName(favitems[i].getid()) << " Profit : " << favitems[i].profit << endl << " Buy Quantity : " << favitems[i].buyquantity() << " Sell Quantity : " << favitems[i].sellquantity() << endl << " Buy Price : " << favitems[i].buyprice() << " Sell Price : " << favitems[i].sellprice() << endl;
+				cout << " TYPE u to UPDATE FETCHED LIST or any key to continue" << endl;
+				cin >> user_in;
+				if (user_in == "u" || user_in == "U")
+				{
+					while (user_in != "esc")
+					{
+						for (int i = 0; i < parsesize; i++) {
+							favitems[i] = ParseJSON_ITEM(favitems[i].getid());
+						}
+						std::sort(favitems, favitems + favj);
+						for (int i = 0; i < parsesize; i++)cout << "[ " << i << " ]" << ParseJSON_ITEMName(favitems[i].getid()) << " Profit : " << favitems[i].profit << endl << " Buy Quantity : " << favitems[i].buyquantity() << " Sell Quantity : " << favitems[i].sellquantity() << endl << " Buy Price : " << favitems[i].buyprice() << " Sell Price : " << favitems[i].sellprice() << endl;
+
+						cout << "Press any key then enter to continue or type esc to stop" << endl;
+						cin >> user_in;
+					}
+				}
 			}
+
 			if (user_in == "3")
 			{
 				cout << " ENTER THE ITEM NUMBER(S) SHOWN IN [] FINISH WITH esc" << endl;
@@ -129,6 +175,7 @@ int main(void) {
 				cout << endl;
 				favselect = true;
 			}
+
 			if (user_in == "2")
 			{
 				if (favselect) {
@@ -147,13 +194,26 @@ int main(void) {
 				}
 				else cout << " Select favourites first!" << endl;
 			}
+
 			if (user_in == "4")
 			{
-				cout << "Current MIN Limits : BUYING QUANTITY " << BUYQ_Limit << " SELLING QUANTITY " << SELLQ_Limit << endl;
-				cout << "Input new limits and separate them with space" << endl;
-				cin >> BUYQ_Limit >> SELLQ_Limit;
+				cout << "What would you like to change? [1] Quantity Limits [2] Price Limits";
+				cin >> user_in;
+				if (user_in == "1")
+				{
+					cout << "Current MIN Limits : BUYING QUANTITY " << BUYQ_Limit << " SELLING QUANTITY " << SELLQ_Limit << endl;
+					cout << "Input new limits and separate them with space" << endl;
+					cin >> BUYQ_Limit >> SELLQ_Limit;
+				}
+				else if (user_in == "2")
+				{
+					cout << "Current Price Limits : MIN PRICE " << BUYG_MIN << " MAX PRICE " << BUYG_MAX << endl;
+					cout << "Input new limits and separate them with space" << endl;
+					cin >> BUYG_MIN >> BUYG_MAX;
+				}
 			}
-			cout << " [1] LIST [2] LIVE UPDATE FAV [3] SELECT FAVOURITES [4] CHANGE SELL QUANTITY LIMITS [5] REBUILD DATABASE [esc] QUIT" << endl;
+
+			cout << " [1] LIST [2] LIVE UPDATE FAV [3] SELECT FAVOURITES [4] CHANGE LIMITS [5] REBUILD DATABASE [esc] QUIT" << endl;
 			cin >> user_in;
 		} while (user_in != "esc");
 		// COMPARISON OF SELL QUANTITIES
@@ -172,7 +232,7 @@ void itemlistload(string filename, int *itemlist)
 	fstream itemliststream(filename,std::ios_base::in);
 	string itemliststring;
 	if (!itemliststream.is_open())cout << "item file not open bob";
-	for (int i = 0; i < 24465; i++)
+	for (int i = 0; i < ITEM_COUNT; i++)
 	{
 		itemliststream >> itemliststring;
 		itemlist[i] = stoi(itemliststring);
@@ -191,16 +251,28 @@ void itemdbload(string filename, item *itemdb)
 		}
 	}
 }
+int itemdbintegrity(string filename)
+{
+	fstream itemdbstream(filename, std::ios_base::in);
+	string itemdbstring;
+	int line = 0;
+	if (!itemdbstream.is_open())return -1;
+	else
+	{
+		while (std::getline(itemdbstream, itemdbstring))++line;
+		return line;
+	}
+}
 //****************************************************
-string ParseJSON(string *inputfile, string tobefound, string tobefoundwithin)
+string ParseJSON(string *inputfile, string tobefound, string tobefoundwithin, string tobefoundwithinfirst)
 {
 	int size, pos;
 	stringstream parsedstringstream;
 	string parsedstring;
 	
 	if(inputfile->find("no such id")!=string::npos)return "nan";
-	size = getstringinputsizeJSON(inputfile, tobefound,tobefoundwithin);
-	pos = getstringinputpositionJSON(inputfile, tobefound, tobefoundwithin);
+	size = getstringinputsizeJSON(inputfile, tobefound,tobefoundwithin,tobefoundwithinfirst);
+	pos = getstringinputpositionJSON(inputfile, tobefound, tobefoundwithin,tobefoundwithinfirst);
 	//cout << size << pos << endl;
 	for (int i = 0; i < size; i++)parsedstringstream << inputfile->at(i + pos);
 	parsedstring = parsedstringstream.str();
@@ -250,6 +322,52 @@ item ParseJSON_ITEM(int it)
 	item anitem(stoi(id),stoi(buy_price),stoi(buy_quantity),stoi(sell_price),stoi(sell_quantity));
 	return anitem;
 }
+void InitJSON_ITEMSDB(item* items,int* itemslist,int itempos)
+{
+	string fixlink = "https://api.guildwars2.com/v2/commerce/prices?ids=";
+	string requestitemid;
+	string currID = "?",bcurrID = "?";
+	string id, buy_quantity, sell_quantity, buy_price, sell_price;
+	//STR to CHAR
+	for (int i = 0+itempos; i < ITEM_REQ_COUNT+itempos && i < ITEM_COUNT; i++)
+	{
+		fixlink.append(to_string(itemslist[i]));
+		fixlink.append(",");
+	}
+	
+	char *newfixlink = new char[fixlink.length() + 1];
+	strcpy_s(newfixlink, fixlink.length() + 1, fixlink.c_str());
+	// MAKE THE CONNECTION
+	stringresponse = "";
+	get_Website(newfixlink);
+
+	//cout << "Response read successfully." << endl;
+	//cout << stringresponse << endl;
+
+	// PARSE RESPONSE 
+	//cout << "Parse Response" << endl;
+	for (int i = 0 + itempos; i < ITEM_REQ_COUNT + itempos && i < ITEM_COUNT; i++)
+	{
+		currID = "";
+		currID.append("\"id\": ");
+		currID.append(to_string(itemslist[i]));
+
+		id = ParseJSON(&stringresponse, "id",currID);
+		buy_quantity = ParseJSON(&stringresponse, "quantity", "buys",currID);
+		buy_price = ParseJSON(&stringresponse, "unit_price", "buys",currID);
+		sell_quantity = ParseJSON(&stringresponse, "quantity", "sells",currID);
+		sell_price = ParseJSON(&stringresponse, "unit_price", "sells",currID);
+		//cout << "FOUND :" << id << " " << buy_price << " " << buy_quantity << " " << sell_price << " " << sell_quantity << endl;
+		//cout << "Item:";
+		bcurrID = currID;
+		//if (i < 99 + itempos && i < ITEM_COUNT - 1)
+		//{
+
+		//}
+		item anitem(stoi(id), stoi(buy_price), stoi(buy_quantity), stoi(sell_price), stoi(sell_quantity));
+		items[i] = anitem;
+	}
+}
 /* int getstringinputsizeJSON (string inputfile, string tobefound)
 @Description  Counts the data size of the variable given in the config file
 @libDependencies - string.h
@@ -264,23 +382,26 @@ item ParseJSON_ITEM(int it)
 @example return = int 11
 
 */
-int getstringinputsizeJSON(string *inputfile, string tobefound,string tobefoundwithin)
+int getstringinputsizeJSON(string *inputfile, string tobefound,string tobefoundwithin, string tobefoundwithinfirst)
 {
 	size_t strpos=0;
 	int size = 0;
-	size_t eolpos;
+	size_t eolpos,eolpos2;
 	size_t datastartpos;
 	
-	if(tobefoundwithin!="?")strpos = inputfile->find(tobefoundwithin);
+	if(tobefoundwithin!="?" && tobefoundwithinfirst =="?")strpos = inputfile->find(tobefoundwithin);
+	if (tobefoundwithinfirst != "?")strpos = inputfile->find(tobefoundwithinfirst);
+	if(tobefoundwithin != "?" && tobefoundwithinfirst != "?")strpos = inputfile->find(tobefoundwithin,strpos);
 	strpos = inputfile->find(tobefound,strpos);
 	datastartpos = inputfile->find(":", strpos);
 	eolpos = inputfile->find(",", datastartpos);
+	eolpos2 = inputfile->find("\n", datastartpos);
 	bool eol = false;
 	for (int i = datastartpos + 2; i < inputfile->size() && !eol; i++)
 	{
 
 		//cout << i;
-		if (i == eolpos)eol = true;
+		if (i == eolpos || i==eolpos2)eol = true;
 		else
 		{
 			size += 1;
@@ -303,12 +424,14 @@ int getstringinputsizeJSON(string *inputfile, string tobefound,string tobefoundw
 @example return = int 11
 
 */
-int getstringinputpositionJSON(string *inputfile, string tobefound, string tobefoundwithin)
+int getstringinputpositionJSON(string *inputfile, string tobefound, string tobefoundwithin, string tobefoundwithinfirst)
 {
 	size_t strpos = 0;
 	int size = 0;
 	size_t eolpos,datastartpos;
 	if (tobefoundwithin != "?")strpos = inputfile->find(tobefoundwithin);
+	if (tobefoundwithinfirst != "?")strpos = inputfile->find(tobefoundwithinfirst);
+	if (tobefoundwithin != "?" && tobefoundwithinfirst != "?")strpos = inputfile->find(tobefoundwithin, strpos);
 	strpos = inputfile->find(tobefound, strpos);
 	datastartpos = inputfile->find(":", strpos);
 	eolpos = inputfile->find(",", datastartpos);
@@ -316,8 +439,7 @@ int getstringinputpositionJSON(string *inputfile, string tobefound, string tobef
 	return datastartpos+2;
 }
 
-// Winsock function that sends http request to the given string url and reads to response to char buffer that is defined globally.
-// RETURN STRING?
+// CURL CODE TO MAKE THE HTTPS REQUESTS
 void get_Website(char *url) {
 	CURL *curl;
 	CURLcode res;
